cb5bf6510253dc774dbacee2f12f3b2b4a8b06ad
Marcin Lazowski <gitforgit123@gmail.com>
2026-07-11 22:25:34 +0200

    feat:adds backend limits

--- server/app.js
+++ server/app.js
@@ -44,7 +44,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use(express.json());
+app.use(express.json({ limit: '100kb' }));
 
 
 app.use('/api', authRoutes); // register/login/userInfo/changeInfo -> /api/register, etc.
